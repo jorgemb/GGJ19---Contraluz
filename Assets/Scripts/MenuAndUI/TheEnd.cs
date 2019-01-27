e8ee4033a8550ccfdce7438d15b42301e8b29c37
Melinton Navas <34899600+MelintonN@users.noreply.github.com>
2019-01-27 15:30:15 -0600

    Ending and stuff

---
+++ Assets/Scripts/MenuAndUI/TheEnd.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TheEnd : MonoBehaviour
+{
+    public float contador;
+    public float onScreenTime = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contador = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (contador > onScreenTime)
+        {
+            SceneManager.LoadScene(1);
+        }
+        contador = contador + Time.deltaTime;
+
+    }
+}
