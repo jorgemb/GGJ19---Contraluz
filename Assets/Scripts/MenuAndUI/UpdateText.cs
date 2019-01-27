a2eb55c63a7775570bb3649a420be64a39fd41d1
Melinton Navas <34899600+MelintonN@users.noreply.github.com>
2019-01-27 13:35:11 -0600

    updateText method on HUD status

---
+++ Assets/Scripts/MenuAndUI/UpdateText.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateText : MonoBehaviour
+{
+    public void updateText(string newText)
+    {
+        GetComponent<Text>().text = newText;
+    }
+}
