88c0e323156712ad4f402a8280f6ca2b5c66acad
Jorge L. Martinez <jorelmb@gmail.com>
2019-01-27 15:19:17 -0600

    Added prefabs for barricade.

---
+++ Assets/Scripts/Controllers/BarricadeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarricadeController : MonoBehaviour
+{
+    AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!audioSource.isPlaying)
+            audioSource.Play();
+    }
+}
