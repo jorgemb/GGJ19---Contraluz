f706d3b254b042af9225c352ac7b41a9e2344d99
Melinton Navas <34899600+MelintonN@users.noreply.github.com>
2019-01-27 13:54:54 -0600

    Flickering for status text

--- Assets/Scripts/MenuAndUI/UpdateText.cs
+++ Assets/Scripts/MenuAndUI/UpdateText.cs
@@ -5,8 +5,42 @@ using UnityEngine.UI;
 
 public class UpdateText : MonoBehaviour
 {
+    private Image imageComponent;
+    private Text hudText;
+    private float ratio;
+
+    public void Start()
+    {      
+        hudText = GetComponent<Text>();
+        updateText("SCARED");
+        ratio = 98.5f;
+    }
     public void updateText(string newText)
     {
-        GetComponent<Text>().text = newText;
+        hudText.text = newText;
+    }
+
+    public void Update()
+    {
+        if(GetComponent<Text>().text == "SCARED" && ratio > 50f)
+        {
+            ratio = 50f;
+        }
+        else if (ratio == 50f)
+        {
+            ratio = 98.5f;
+        }
+
+        if (Random.Range(1f, 100f) > ratio)
+        {
+            hudText.color = Color.black;
+        }
+        else
+        {
+            if (hudText.color != Color.green)
+            {
+                hudText.color = Color.green;
+            }
+        }
     }
 }
