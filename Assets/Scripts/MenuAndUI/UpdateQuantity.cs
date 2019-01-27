460eaf36edf5b38ae718c8b8d187b4afa46510ae
Melinton Navas <34899600+MelintonN@users.noreply.github.com>
2019-01-27 14:25:50 -0600

    HUD QTY and text updates

---
+++ Assets/Scripts/MenuAndUI/UpdateQuantity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateQuantity : MonoBehaviour
+{
+    private Text textComponent;
+
+    private void Start()
+    {
+        textComponent = GetComponent<Text>();
+    }
+
+    public void updateNumber(int qty)
+    {
+        string newText;
+        try
+        {
+            newText = qty.ToString();
+            textComponent.text = newText;
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.StackTrace);
+        }
+    }
+}
